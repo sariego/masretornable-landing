3d49fde2cf270a0764d1a94b2e73d90d7df13428
sariego <pedro@sariego.dev>
2021-10-08 04:57:07 -0300

    initial status

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'green-brand': '#36A764',
+        'blue-brand': '#5F9CF7',
+      },
+      fontFamily: {
+        spartan: ['League Spartan', 'sans-serif'],
+        open: ['Open Sans Light', 'sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
